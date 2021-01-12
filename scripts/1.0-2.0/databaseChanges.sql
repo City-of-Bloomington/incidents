3de1042ef00509a414cce3c5632d9f0c4b6d2307
Cliff Ingham <inghamn@bloomington.in.gov>
2021-01-12 15:51:34 -0500

    Started database migration script
    This should take a database from the old PHP application and bring it up to date with the new Java
    version.
    
    Updates #1

---
+++ scripts/1.0-2.0/databaseChanges.sql
@@ -0,0 +1,127 @@
+alter table users drop password;
+alter table users drop authenticationMethod;
+alter table users add  inactive char(1);
+
+alter table roles rename column role to name;
+
+rename table statuses to actions;
+alter table actions add nextstep tinyint unsigned;
+update actions set nextstep=2 where name='received';
+update actions set nextstep=2 where name='confirmed';
+update actions set nextstep=3 where name='approved';
+insert actions set name='emailed',description='Incomplete, Email Sent',workflow_step=1;
+
+rename table status_history to action_logs;
+alter table action_logs rename column status_id to action_id;
+
+rename table status_roles to action_roles;
+alter table action_roles rename column status_id to action_id;
+
+rename table carDamageTypes to car_damage_types;
+alter table car_damage_types rename column type to name;
+
+rename table damageTypes to damage_types;
+alter table damage_types rename column type to name;
+alter table damage_types add inactive char(1);
+
+-- Incidents
+rename table incidentTypes to incident_types;
+alter table incident_types change type name varchar(70) not null;
+
+alter table incidents rename column cfsNumber       to case_number;
+alter table incidents rename column incidentType_id to incident_type_id;
+alter table incidents rename column status_id       to action_id;
+alter table incidents rename column dateDescription to date_description;
+alter table incidents rename column haveMedia       to have_media;
+alter table incidents modify entry_type enum('Unlocked vehicle','Broke window','Pried window','Pried door','Other specify','Damaged window','Damaged door','Unlocked door');
+
+-- Addresses
+create table addresses (
+  id           int unsigned not null primary key auto_increment,
+  name         varchar(80)  not null unique,
+  latitude     decimal(15,10),
+  longitude    decimal(15,10),
+  city         varchar(80),
+  state        varchar(2),
+  zipcode      varchar(15),
+  jurisdiction varchar(80),
+  address_id   int unsigned,
+  subunit_id   int unsigned,
+  invalid_address char(1)
+);
+
+insert into addresses (name, city, state, zipcode, invalid_address)
+select address, city, state, zip, invalidAddress from incidents i
+on duplicate key update city=i.city, state=i.state, zipcode=i.zip, invalid_address=i.invalidAddress;
+
+alter table incidents add address_id int unsigned after date;
+alter table incidents add foreign key (address_id) references addresses(id);
+update incidents i set i.address_id=(select a.id from addresses a where a.name=i.address);
+
+alter table incidents drop column address;
+alter table incidents drop column city;
+alter table incidents drop column state;
+alter table incidents drop column zip;
+alter table incidents drop column invalidAddress;
+alter table incidents add evidence text after details;
+
+
+-- Media
+alter table media rename column name to file_name;
+
+-- Persons
+rename table personTypes to person_types;
+
+alter table persons rename column personType_id to person_type_id;
+alter table persons rename column phonetype     to phone_type;
+alter table persons add phone_type2 enum('Cell', 'Home', 'Work') after phone_type;
+alter table persons rename column driverLicenseNumber  to dln;
+alter table persons rename column dateOfBirth          to dob;
+alter table persons rename column socialSecurityNumber to ssn;
+alter table persons rename column heightFeet to height_feet;
+alter table persons rename column heightInch to height_inch;
+alter table persons modify sex enum('Male', 'Female', 'Nonbinary', 'Unknown');
+alter table persons add gender enum('Male', 'Female', 'Trangender') after sex;
+alter table persons add ethnicity enum('Hispanic','Non-hispanic','Unknown') after gender;
+
+create table race_types (
+    id int unsigned not null primary key auto_increment,
+    name varchar(50) not null unique
+);
+insert into race_types (name) select distinct race from persons where race is not null;
+alter table persons add race_type_id int unsigned after race;
+alter table persons add foreign key (race_type_id) references race_types(id);
+update persons set race_type_id=(select id from race_types where name=race);
+alter table persons drop column race;
+
+-- Properties
+alter table properties rename column damageType_id to damage_type_id;
+alter table properties rename column serialNum     to serial_num;
+
+-- Vehicles
+alter table vehicles rename column carDamageType_id   to car_damage_type_id;
+alter table vehicles rename column licensePlateNumber to plate_number;
+alter table vehicles rename column licensePlateYear   to plate_year;
+alter table vehicles add value decimal(10, 2);
+
+
+-- ----------
+-- New Tables
+-- ----------
+create table fraud_types (
+    id int unsigned not null primary key auto_increment,
+    name varchar(50) not null unique
+);
+
+create table frauds (
+  id            int unsigned not null primary key auto_increment,
+  incident_id   int unsigned not null,
+  fraud_type_id int unsigned,
+  other_type    varchar(80) default null,
+  identity_used text,
+  account_used  text,
+  amount_taken  decimal(12,2) default null,
+  details       text,
+  foreign key (incident_id)   references incidents(id),
+  foreign key (fraud_type_id) references fraud_types(id)
+);
